ca47327d6be0d290d1cf489f8569b1d98f52ae2f
kisday <codeforyz@gmail.com>
2017-05-13 14:38:06 -0400

    add bootstrap

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <template v-for="variant in ['primary','secondary','success','outline-success','warning','danger','link']">
+        <div class="col-md-4 pb-2" v-for="size in ['sm','','lg']">
+          <b-button :size="size" :variant="variant" href="">
+            {{variant}} {{size}}
+          </b-button>
+        </div>
+      </template>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
